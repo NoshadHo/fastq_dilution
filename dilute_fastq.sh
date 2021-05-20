ee205fe66dd02b28bb174d9719823eb697e0f35d
noshadho <noshadho@gmail.com>
2021-05-20 19:18:22 -0400

    initiation

---
+++ dilute_fastq.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function dilute_fastq {
+#   $1 is id
+#   $2 is t.fastq1 location
+#   $3 is t.fastq2 location
+#   $4 is n.fastq1 location
+#   $5 is n.fastq2 location
+#   $6 is write location
+#   $7 dilution percent (x% means x part tumor and 1-x part normal)
+#   $8 depth percent (what percentage of minimum tomor/normal as total depth)
+
+
+
+
+}
